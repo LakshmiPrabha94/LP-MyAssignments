cdb4f1a8e81de4e2f28b4f5a31c941ece5507741
LakshmiPrabha94 <137052069+LakshmiPrabha94@users.noreply.github.com>
2023-06-19 14:52:24 +0530

    Add files via upload
    Day1 - Class Exercise
    1. FirstProgram
    2. Chrome
    3. Browser

---
+++ day1/Chrome.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Chrome {
+	public void getName() {
+		System.out.println("This is Google Chrome");
+	}
+	public static void main(String[] args) {
+		System.out.println("Methods - Tutorial");
+		Chrome g = new Chrome();
+		g.getName();
+	}
+}
---
+++ day1/FirstProgram.java
@@ -0,0 +1,8 @@
+package week1.day1;
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Glad to be a student of Testleaf org :)");
+		System.out.println("Kudos to the Team");
+	}
+}
