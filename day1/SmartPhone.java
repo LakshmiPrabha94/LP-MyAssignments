96d268873e81a952cbeffbca38edb969b02e0de8
LakshmiPrabha94 <137052069+LakshmiPrabha94@users.noreply.github.com>
2023-06-19 15:50:09 +0530

    Add files via upload

---
+++ day1/SmartPhone.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class SmartPhone {
+
+	public static void main(String[] args) {
+		System.out.println("IPhone function call using objects from class Smart phone");
+		IPhone i = new IPhone();
+		i.makeCall();
+		i.model();
+	}
+
+}
