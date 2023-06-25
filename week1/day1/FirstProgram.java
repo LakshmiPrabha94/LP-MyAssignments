04754bd13b383cb0e6789c88db353016fe20ac85
LakshmiPrabha94 <137052069+LakshmiPrabha94@users.noreply.github.com>
2023-06-25 19:52:10 +0530

    Delete week1 directory

--- week1/day1/FirstProgram.java
+++
@@ -1,8 +0,0 @@
-package week1.day1;
-
-public class FirstProgram {
-	public static void main(String[] args) {
-		System.out.println("Glad to be a student of Testleaf org :)");
-		System.out.println("Kudos to the Team");
-	}
-}
